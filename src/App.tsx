5c2c82b435f67a4b50707b002ed55f7c1f106f84
thaone2 <khuong123w@gmail.com>
2024-05-06 12:02:35 +0700

    comment code shadcn ui 3

--- src/App.tsx
+++ src/App.tsx
@@ -5,24 +5,6 @@ import {
   SiTiktok,
   SiGmail,
 } from "react-icons/si";
-// import { Button } from "@/components/ui/button";
-// import {
-//   Card,
-//   CardContent,
-//   CardDescription,
-//   CardFooter,
-//   CardHeader,
-//   CardTitle,
-// } from "@/components/ui/card";
-// import { Input } from "@/components/ui/input";
-// import { Label } from "@/components/ui/label";
-// import {
-//   Select,
-//   SelectContent,
-//   SelectItem,
-//   SelectTrigger,
-//   SelectValue,
-// } from "@/components/ui/select";
 
 import * as React from "react";
 import avatar from "./image/avatar.jpg";
@@ -109,42 +91,6 @@ function App() {
               <li> React JS lỏ</li>
             </ul>
           </div>
-          {/* <Card className=" m-4 border shadow-sm rounded-xl shadow-green-500">
-            <CardHeader>
-              <CardTitle>Create project</CardTitle>
-              <CardDescription>
-                Deploy your new project in one-click.
-              </CardDescription>
-            </CardHeader>
-            <CardContent>
-              <form>
-                <div className="grid w-full items-center gap-4">
-                  <div className="flex flex-col space-y-1.5">
-                    <Label htmlFor="name">Name</Label>
-                    <Input id="name" placeholder="Name of your project" />
-                  </div>
-                  <div className="flex flex-col space-y-1.5">
-                    <Label htmlFor="framework">Framework</Label>
-                    <Select>
-                      <SelectTrigger id="framework">
-                        <SelectValue placeholder="Select" />
-                      </SelectTrigger>
-                      <SelectContent position="popper" className="bg-white">
-                        <SelectItem value="next">Next.js</SelectItem>
-                        <SelectItem value="sveltekit">SvelteKit</SelectItem>
-                        <SelectItem value="astro">Astro</SelectItem>
-                        <SelectItem value="nuxt">Nuxt.js</SelectItem>
-                      </SelectContent>
-                    </Select>
-                  </div>
-                </div>
-              </form>
-            </CardContent>
-            <CardFooter className="flex justify-between">
-              <Button variant="outline">Cancel</Button>
-              <Button className="border border-black">Deploy</Button>
-            </CardFooter>
-          </Card> */}
         </div>
       </div>
       {/* FOOTER */}
